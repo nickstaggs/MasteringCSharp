5f497cdb39fbb72fdea32a3ca13afa733fa93880
Nick Staggs <nick.c.staggs@gmail.com>
2018-08-10 09:50:52 -0400

    Initial Commit

---
+++ MasteringCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace MasteringCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic x = "hello";
+            Console.WriteLine(x.Length);
+            x = new int[] { 10, 20, 30 };
+            Console.WriteLine(x.Length);
+
+            Thread.Sleep(2000);
+        }
+
+        static void foo(string x)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
