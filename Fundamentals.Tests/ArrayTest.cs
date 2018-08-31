d59d2cbbaa43aa5631774ce31c577ced02dd165a
Nick Staggs <nick.c.staggs@gmail.com>
2018-08-31 14:58:42 -0400

    Through arrays

---
+++ Fundamentals.Tests/ArrayTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Fundamentals.Tests
+{
+    [TestFixture]
+    class ArrayTest
+    {
+        [Test]
+        public void ArrayValuesAreReferenceTypes()
+        {
+            int[] array = new int[10];
+
+            array[0] = 5;
+            AddOne(ref array[0]);
+
+            Assert.AreEqual(array[0], 6);
+
+            AddOneTo0thElement(array);
+
+            Assert.AreEqual(array[0], 7);
+        }
+
+        public void AddOne(ref int val)
+        {
+            val++;
+        }
+
+        public void AddOneTo0thElement(int[] arr)
+        {
+            arr[0]++;
+        }
+    }
+}
