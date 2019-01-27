0b5338b1ca494ce7354809662c577a28c0bd2a66
Nick Staggs <nick.c.staggs@gmail.com>
2019-01-26 20:19:54 -0500

    Added Hashmap

---
+++ MasteringCSharp/HashMap/HashMap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace MasteringCSharp.HashMap
+{
+    public class HashMap<TKey, TValue> : IHashMap<TKey, TValue>, ICollection
+    {
+        public HashMap()
+        {
+        }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue Get(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TValue> GetAsync(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetAsync(TKey key, TValue value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MasteringCSharp/HashMap/IHashMap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MasteringCSharp.HashMap
+{
+    public interface IHashMap<TKey, TValue>
+    {
+        TValue Get(TKey key);
+        void Set(TKey key, TValue value);
+        Task<TValue> GetAsync(TKey key);
+        Task SetAsync(TKey key, TValue value);
+
+    }
+}
